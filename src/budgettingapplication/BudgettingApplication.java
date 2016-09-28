8515fd651bebaa5a3c87518e3f4f621e75cddf26
Esg450 <evan.gentis@gmail.com>
2016-09-28 15:27:43 -0400

    Initial commit

---
+++ src/budgettingapplication/BudgettingApplication.java
@@ -0,0 +1,16 @@
+package budgettingapplication;
+
+/**
+ *
+ * @author evangentis
+ */
+public class BudgettingApplication {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
