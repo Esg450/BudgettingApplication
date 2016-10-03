760d55b34f3dfdfe1eb55d6176a6441ec9d18e5b
dak5332 <dak5332@110-WL30.up.ist.local>
2016-10-03 14:40:10 -0400

    added transaction class

--- src/budgettingapplication/Transaction.java
+++ src/budgettingapplication/Transaction.java
@@ -5,10 +5,56 @@
  */
 package budgettingapplication;
 
+import java.util.Date;
+
 /**
  *
  * @author dylankolson
  */
 public class Transaction {
+    private String name;
+    private double amount;
+    private String type;
+    private Date date; 
+
+    public Transaction(String name, double amount, String type, Date date) {
+        this.name = name;
+        this.amount = amount;
+        this.type = type;
+        this.date = date;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+    
     
 }
