4c3ad578a4866eb15b86dec05cc31018e7d77338
Esg450 <evan.gentis@gmail.com>
2016-10-09 15:18:14 -0400

    Update dash

--- src/budgettingapplication/DashboardUI.java
+++ src/budgettingapplication/DashboardUI.java
@@ -89,11 +89,13 @@ public class DashboardUI extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        // TODO add your handling code here:
+        BudgetController bcontrol = new BudgetController();
+        bcontrol.showLoginUI();
     }//GEN-LAST:event_jButton1ActionPerformed
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
-        // TODO add your handling code here:
+        TransactionController transcontrol = new TransactionController();
+        transcontrol.showLoginUI();
     }//GEN-LAST:event_jButton2ActionPerformed
 
     /**
